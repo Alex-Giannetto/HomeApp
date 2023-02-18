90d1d8153777f07eb16b178c9d1f250f3a1dda32
Alex <alexg78bis@gmail.com>
2023-02-18 16:55:51 +0100

    Initial Commit

---
+++ Home/HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeApp.swift
+//  Home
+//
+//  Created by Ålex on 18/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
